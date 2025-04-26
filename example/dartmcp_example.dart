034612387521259a7a11614927c99f674e18daf9
Jeevan Joshi <joshijeevan98@gmail.com>
2025-04-26 21:58:05 +0530

    setup library

---
+++ example/dartmcp_example.dart
@@ -0,0 +1,6 @@
+import 'package:dartmcp/dartmcp.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dartmcp_test.dart
@@ -0,0 +1,16 @@
+import 'package:dartmcp/dartmcp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
